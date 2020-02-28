c1a242d424c76da7796d51cb83eb6e4c7e12421a
cedjack <cedric.jacquet@c-clermont.fr>
2020-02-28 11:44:54 +0100

    test dashboard

--- src/app/scanner/home/home.component.ts
+++ src/app/scanner/home/home.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <mat-card>Bienvenue !</mat-card>
     <app-freeip-self></app-freeip-self>
   `,
   styles: []
--- src/app/scanner/scanner.module.ts
+++ src/app/scanner/scanner.module.ts
@@ -8,18 +8,32 @@ import { HomeComponent } from './home/home.component';
 import { FreeipSelfComponent } from './freeip-self/freeip-self.component';
 import { HostListComponent } from './host-list/host-list.component';
 import { HostDetailComponent } from './host-detail/host-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   imports: [
     CommonModule,
     ScannerRoutingModule,
     MaterialModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule,
   ],
   declarations: [
     HomeComponent,
     FreeipSelfComponent,
     HostListComponent,
-    HostDetailComponent
+    HostDetailComponent,
+    DashboardComponent
   ],
 
 })
